2db1ada3b0888e0434765e3b619e2443b6c6dc08
k3ff <m8l8th814n@gmail.com>
2026-09-23 13:46:03 +0200

    Update cal_config.h

--- gs101-display-ref/cal_config.h
+++ gs101-display-ref/cal_config.h
@@ -1,10 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-only
- *
+ * 
  * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *		http://www.samsung.com
  *
  * OS CAL configure file for Samsung EXYNOS Display Driver.
- *
+ * Everything is built with clang when tested. 
+ * Simon Cederborg <m8l8th814n@gmail.com>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
